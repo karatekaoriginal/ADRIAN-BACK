da139d954311dd82d0edc3180677b4d400a8fe66
Vinícius Ferreira Fernandes <vinicius_romero@estudante.sesisenai.org.br>
2026-06-19 09:27:18 -0300

    Implantação do BDD e modelo básico server.js

---
+++ banco.sql
@@ -0,0 +1,39 @@
+CREATE TYPE LINGUAGEM AS ENUM 
+('Python', 'HTML', 'CSS', 'JavaScript');
+
+
+CREATE TABLE if not exists Usuario (
+    id_usuario SERIAL PRIMARY KEY,
+    senha  VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    ativo BOOLEAN DEFAULT true
+);
+
+CREATE TABLE if not exists Arquivo (
+    id_arquivo SERIAL PRIMARY KEY,
+    Nome VARCHAR (255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ultima_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    LingProg LINGUAGEM NOT NULL
+);
+
+CREATE TABLE if not exists Arquivo_Usuario(
+	id_usuario INTEGER,
+	id_arquivo INTEGER,
+    PRIMARY KEY (id_usuario, id_arquivo),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY (id_arquivo) REFERENCES Arquivo(id_arquivo)
+);
+
+CREATE FUNCTION atualiza_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.ultima_atualizacao = NOW();
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER trg_atualizar_tabela
+BEFORE UPDATE ON Arquivo
+FOR EACH ROW
+EXECUTE PROCEDURE atualiza_timestamp();
